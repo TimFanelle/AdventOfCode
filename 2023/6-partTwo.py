6290b4a122eebf57d9db335389a04a5861b3ecb5
TimFanelle <fanelle.tim@gmail.com>
2023-12-06 07:05:03 -0800

    completed day 6 parts 1 and 2

---
+++ 2023/6-partTwo.py
@@ -0,0 +1,30 @@
+lens = []
+with open("AdventOfCode/2023/6-input.txt", 'r') as textIn:
+	vals = []
+	for line in textIn:
+		l = line.split(":")[1].split()
+		st = ""
+		for u in l:
+			st += u
+		nums = [int(st)]
+		vals.append(nums)
+	vals = [list(l) for l in zip(*vals)]
+	for item in vals:
+		low_found = False
+		high_found = False
+		start = item[0]
+		dist = item[1]
+		low = 0
+		high = item[0]
+		while not high_found:
+			high -= 1
+			if high * (start - high) > dist:
+				high_found = True
+		while not low_found:
+			low += 1
+			if low * (start - low) > dist:
+				low_found = True
+		lens.append(high - low + 1)
+
+from functools import reduce
+print(reduce((lambda x,y: x*y), lens))
