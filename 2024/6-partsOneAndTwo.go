5078b7cdcd4affb32da3e2bbb90e84e0056a9ccf
TimFanelle <fanelle.tim@gmail.com>
2024-12-06 09:14:18 -0800

    fixed part 2

--- 2024/6-partsOneAndTwo.go
+++ 2024/6-partsOneAndTwo.go
@@ -131,43 +131,86 @@ func determineObstacles(input [][]string, curLoc guardLoc) int {
 	return numObs
 }
 
+func move(curLoc guardLoc) guardLoc {
+	switch curLoc.direction {
+		case 4:
+			curLoc.col -= 1
+		case 2:
+			curLoc.col += 1
+		case 1:
+			curLoc.row -= 1
+		case 3:
+			curLoc.row += 1
+	}
+	return curLoc
+}
+
+func nextIsUnobstructed(input [][]string, curLoc guardLoc) bool {
+	switch curLoc.direction {
+		case 4:
+			if curLoc.col -1 >= 0 && input[curLoc.row][curLoc.col-1] != "#"{
+				return true
+			} else if curLoc.col - 1 < 0 {
+				return true
+			}
+		case 2:
+			if curLoc.col +1 < len(input[curLoc.row]) && input[curLoc.row][curLoc.col+1] != "#"{
+				return true
+			}else if curLoc.col + 1 >= len(input[curLoc.row]) {
+				return true
+			}
+		case 1:
+			if curLoc.row-1 >=0 && input[curLoc.row-1][curLoc.col] != "#" {
+				return true
+			} else if curLoc.row - 1 < 0 {
+				return true
+			}
+		
+		case 3:
+			if curLoc.row + 1 < len(input) && input[curLoc.row+1][curLoc.col] != "#"{
+				return true
+			} else if curLoc.row + 1 >= len(input) {
+				return true
+			}
+		default:
+			return false
+	}
+	return false
+}
+
+func turn(curLoc guardLoc) int {
+	switch curLoc.direction {
+		case 4:
+			return 1
+		case 2:
+			return 3
+		case 1:
+			return 2
+		case 3:
+			return 4
+	}
+	return 0
+}
+
 func determineIfLoop(input [][]string, curLoc guardLoc) bool {
 	allLocs := make(map[loc]bool);
 	
 	validLoc := curLoc.col < len(input[0]) && curLoc.col >= 0 && curLoc.row >= 0 && curLoc.row < len(input)
+	allLocs[loc{x: curLoc.col, y: curLoc.row, facing_dir: curLoc.direction}] = true
 	
 	for validLoc {
-		_, ok := allLocs[loc{x: curLoc.col, y: curLoc.row, facing_dir: curLoc.direction}]
-		if !ok {
-			allLocs[loc{x: curLoc.col, y: curLoc.row, facing_dir: curLoc.direction}] = true
+		nextValid := nextIsUnobstructed(input, curLoc)
+		if nextValid {
+			curLoc = move(curLoc)
 		} else {
+			curLoc.direction = turn(curLoc)
+		}
+		_, ok := allLocs[loc{x: curLoc.col, y: curLoc.row, facing_dir: curLoc.direction}]
+		if ok {
 			return true
+		} else {
+			allLocs[loc{x: curLoc.col, y: curLoc.row, facing_dir: curLoc.direction}] = true
 		}
-
-		switch curLoc.direction {
-			case 4:
-				curLoc.col -= 1
-				if curLoc.col -1 >= 0 && input[curLoc.row][curLoc.col-1] == "#"{
-					curLoc.direction = 1
-				}
-			case 2:
-				curLoc.col += 1
-				if curLoc.col +1 < len(input[curLoc.row]) && input[curLoc.row][curLoc.col+1] == "#"{
-					curLoc.direction = 3
-				}
-			case 1:
-				curLoc.row -= 1
-				if curLoc.row-1 >=0 && input[curLoc.row-1][curLoc.col] == "#" {
-					curLoc.direction = 2
-				}
-			
-			case 3:
-				curLoc.row += 1
-				if curLoc.row + 1 < len(input) && input[curLoc.row+1][curLoc.col] == "#"{
-					curLoc.direction = 4
-				}
-		}
-			
 		validLoc = curLoc.col < len(input[0]) && curLoc.col >= 0 && curLoc.row >= 0 && curLoc.row < len(input)
 	}
 	return false
